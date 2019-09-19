19f003c67939ad6086d7e82b1c0c7de33415755f
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-19 21:26:06 +0530

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,15 +11,6 @@ if Rails.env.production?
   end
 end
 
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.fog_credentials = {
-#       # Configuration for Amazon S3
-#       :provider              => 'AWS',
-#       :aws_access_key_id     => ENV['AKIA35CGNHAN7O25T3K6'],
-#       :aws_secret_access_key => ENV['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']
-#     }
-#     config.fog_directory     =  ENV['us-east-2']
-#     config.fog_provider = 'fog/aws'
-#   end
-# end
+# heroku config:set S3_ACCESS_KEY="AKIA35CGNHAN7O25T3K6"
+# heroku config:set S3_SECRET_KEY="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"
+# heroku config:set S3_BUCKET="us-east-2"
